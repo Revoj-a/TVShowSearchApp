47c684ba13def721943701232d6eb482c44b2bab
Revoj-a <revojjoever@gmail.com>
2025-08-26 20:05:48 +0800

    async function for getting the data

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const form = document.querySelector("searchForm");
-form.addEventListener("submit", function (e) {
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
-  console.log("Form submitted!");
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  console.log(res.data);
 });
